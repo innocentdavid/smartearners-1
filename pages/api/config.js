e530b805095f16266c3415ca2086877184ff031c
innocentdavid <paulinnocent04@gmail.com>
2022-09-05 13:09:21 +0100

    sdsd

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 const config = {
-  dataset: process.env.SANITY_STUDIO_API_DATASET,
-  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: '2021-08-31', // use a UTC date string
   useCdn: false, // process.env.NODE_ENV === 'production'
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
