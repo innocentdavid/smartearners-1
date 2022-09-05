e485808cac08ce802d9421ebb32bb828fd752a86
innocentdavid <paulinnocent04@gmail.com>
2022-09-05 14:23:36 +0100

    jdj

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -41,43 +41,42 @@ export default async function createUser(req, res) {
           console.log(newUser)
   
           // const create bonus Order = 
-          await client.create({
-            _type: 'order',
-            planId: 'drafts.a73d9975-f00c-4078-81c2-209ac7776584',
-            planTitle: 'Signup Bonus',
-            percentage: 1,
-            da: 5000,
-            dr: 50,
-            active: true,
-            returnPeriod: 365,
-            drTime: newUser?._createdAt,
-            userId: newUser?._id,
-            userTel: newUser?.tel
-          })
+          // await client.create({
+          //   _type: 'order',
+          //   planId: 'drafts.a73d9975-f00c-4078-81c2-209ac7776584',
+          //   planTitle: 'Signup Bonus',
+          //   percentage: 1,
+          //   da: 5000,
+          //   dr: 50,
+          //   active: true,
+          //   returnPeriod: 365,
+          //   drTime: newUser?._createdAt,
+          //   userId: newUser?._id,
+          //   userTel: newUser?.tel
+          // })
   
           // const createBalanceRecord = 
-          await client.create({
-            _type: 'record',
-            title: 'Signup Bonus',
-            category: 'balanceRecord',
-            type: 'income',
-            amount: 300,
-            remaining: 300,
-            userId: newUser._id,
-            userTel: newUser.tel,
-          })
+          // await client.create({
+          //   _type: 'record',
+          //   title: 'Signup Bonus',
+          //   category: 'balanceRecord',
+          //   type: 'income',
+          //   amount: 300,
+          //   remaining: 300,
+          //   userId: newUser._id,
+          //   userTel: newUser.tel,
+          // })
   
-          if (rf) {
-            // const create_Referral_Record = 
-            await client.create({
-              _type: 'referral',
-              user: { _type: 'reference', _ref: newUser?._id, },
-              referrer: { _type: 'reference', _ref: rf, },
-            }).catch(err => {
-              console.log(err)
-            })
-          }
-  
+          // if (rf) {
+          //   // const create_Referral_Record = 
+          //   await client.create({
+          //     _type: 'referral',
+          //     user: { _type: 'reference', _ref: newUser?._id, },
+          //     referrer: { _type: 'reference', _ref: rf, },
+          //   }).catch(err => {
+          //     console.log(err)
+          //   })
+          // }  
         // } catch (err) {
         //   console.error(err)
         //   return res.status(500).json({ message: `Couldn't create user`, err })
